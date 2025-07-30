00a208d97fb1a16f18ec268a8a4fa4eb1fae9d4e
Eddihh36-GIT <eddihh36@gmx.de>
2025-07-30 15:25:17 +0200

    Update YellowCircle.jsx

--- src/assets/YellowCircle.jsx
+++ src/assets/YellowCircle.jsx
@@ -1,1 +1,13 @@
+function YellowCircle() {
+  return (
+    <div style={{
+      width: '100px',
+      height: '100px',
+      backgroundColor: 'yellow',
+      borderRadius: '50%',
+      margin: '20px auto'
+    }} />
+  );
+}
 
+export default YellowCircle;
